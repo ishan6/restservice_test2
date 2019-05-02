2ddb225b88746b41dc1319f383f06af5382b46e6
ishan6 <ishanmenaka6@gmail.com>
2019-05-02 20:28:34 +0530

    Pizza Main Page

--- app/src/main/java/com/example/restservice_test2/MainActivity.java
+++ app/src/main/java/com/example/restservice_test2/MainActivity.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ProductAdapter.OnItemClickListner {
 
-    private static final String URL_DATA = "http://192.168.42.212:8080/demo/all";
+    private static final String URL_DATA = "http://192.168.42.49:8080/demo/all";
 
     RecyclerView recyclerView;
     ProductAdapter adapter;
